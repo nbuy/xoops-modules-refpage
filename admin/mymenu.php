2678b7e7b50462ed6e69e5fa195161f47c8649bc
Nobuhiro YASUTOMI <nobuhiro.yasutomi@nifty.ne.jp>
2023-02-07 16:20:27 +0900

    PHP object handle reference by default.

--- admin/mymenu.php
+++ admin/mymenu.php
@@ -90,7 +90,7 @@ foreach( array_keys( $adminmenu ) as $i ) {
 if ( $use_altsys ) {
     // display
     require_once XOOPS_ROOT_PATH.'/class/template.php' ;
-    $tpl =& new XoopsTpl() ;
+    $tpl = new XoopsTpl() ;
     $tpl->assign( array(
                         'adminmenu' => $adminmenu ,
                         ) ) ;
